5027775c663a97fbe5512d9f69a14df90d0fcf53
LUV SINGH <luv13singh@gmail.com>
2024-02-13 12:00:47 +0530

    update

--- core/ai.py
+++ core/ai.py
@@ -176,37 +176,35 @@ def audio_chat(chat_id, audio_file):
     assistant_message, history =  chat(chat_id, input_message)
     return assistant_message, history
 
-def bhashini_chat(chat_id, message):
+def bhashini_chat(chat_id, input_message):
     history = get_redis_value(chat_id)
     if history == None:
         history = {
-            "thread_id" : None,
-            "run_id" : None,
-            "status" : None,
+            "thread_id": None,
+            "run_id": None,
+            "status": None,
         }
     else:
         history = json.loads(history)
     thread_id = history.get("thread_id")
     run_id = history.get("run_id")
     status = history.get("status")
-    
-    if thread_id and run_id:
-            run = client.beta.threads.runs.retrieve(thread_id, run_id)
-            thread_id = thread.id
-    else:
-        run = None
 
-    if thread_id:
-        try:
-            thread = client.beta.threads.retrieve(thread_id)
-        except Exception as e:
-            thread = create_thread(client)
-            thread_id = thread.id
-    
+    try:
+        run = client.beta.threads.runs.retrieve(thread_id, run_id)
+    except Exception as e:
+        run = None
+    try:
+        thread = client.beta.threads.retrieve(thread_id)
+        thread_id = thread.id
+    except Exception as e:
+        thread = create_thread(client)
+        thread_id = thread.id
+
     if status == "completed" or status == None:
-        run = upload_message(client, thread_id, message, assistant.id)
+        run = upload_message(client, thread_id, input_message, assistant.id)
         run, status = get_run_status(run, client, thread)
-        
+
         resp = get_assistant_message(client, thread_id)
         # this has to be in chosen language
         try:
@@ -218,21 +216,21 @@ def bhashini_chat(chat_id, message):
         print('assistant_message:', assistant_message)  # Print the value of assistant_message
         print('ok6')
         
+
         history = {
-            "thread_id" : thread_id,
+            "thread_id": thread_id,
             "run_id": run.id,
-            "status": status.id
+            "status": status,
         }
-        history = json.dumps(history)
-        set_redis(chat_id, history)
-        
+        set_redis(chat_id, json.dumps(history))
+    
     if status == "requires_action":
         if run:
-            tools_to_call = run.required_action.submit_tools_outputs.tool_calls
+            tools_to_call = run.required_action.submit_tool_outputs.tool_calls
         else:
             run, status = get_run_status(run, client, thread)
-            tools_to_call = run.required_action.submit_tool_outputs.tools_calls
-            
+            tools_to_call = run.required_action.submit_tool_outputs.tool_calls
+
         for tool in tools_to_call:
             username = USERNAME
             auth_token = get_auth_token(
@@ -242,75 +240,74 @@ def bhashini_chat(chat_id, message):
                 }
             )
             func_name = tool.function.name
-            print(f"Function name is {func_name}")
+            print(f"Function name: {func_name}")
             parameters = json.loads(tool.function.arguments)
             parameters["auth_token"] = auth_token
             parameters["username"] = username
             print(f"Parameters: {parameters}")
-            
+
             tool_output_array = []
-            
+
             if func_name == "raise_complaint":
                 complaint = file_complaint(parameters)
                 if complaint:
                     tool_output_array.append(
                         {
-                            "tool_call_id" : tool.id,
-                            "output" : complaint["ServiceWrappers"][0]["service"]["applicationStatus"]
+                            "tool_call_id": tool.id,
+                            "output": complaint["ServiceWrappers"][0]["service"]["serviceRequestId"]
                         }
-                    )            
-                    run = client.beta.threads.tuns.submit_tool_outputs(
-                        thread_id = thread.id,
-                        run_id = run.id,
-                        tool_outputs = tool_output_array
                     )
-                    run,status = get_run_status(run, client, thread)
-                    
+                    run = client.beta.threads.runs.submit_tool_outputs(
+                        thread_id=thread_id,
+                        run_id=run.id,
+                        tool_outputs=tool_output_array
+                    )
+                    run, status = get_run_status(run, client, thread)
+
                     resp1 = get_assistant_message(client, thread_id)
                     message = bhashini_output(resp1, lang=lang)
                     print('ok8')
-                    
+
                     history = {
                         "thread_id": thread.id,
                         "run_id": run.id,
                         "status": status,
                     }
-                    history = json.dumps(history)  
-                    set_redis(chat_id, history)
+                    set_redis(chat_id, json.dumps(history))
                     return message, history
                 else:
-                    return "Complaint failed", history  
+                    return "Complaint failed", history
                 
             elif func_name == "search_complaint":
                 complaint = search_complaint(parameters)
                 if complaint:
                     tool_output_array.append(
                         {
-                            "tool_call_id" : tool.id,
-                            "output" : complaint["ServiceWrappers"][0]["service"]["applicationStatus"]
+                            "tool_call_id": tool.id,
+                            "output": complaint["ServiceWrappers"][0]["service"]["applicationStatus"]
                         }
                     )
                     run = client.beta.threads.runs.submit_tool_outputs(
-                        thread_id = thread.id,
-                        run_id = run.id,
-                        tool_outputs = tool_output_array
+                        thread_id=thread.id,
+                        run_id=run.id,
+                        tool_outputs=tool_output_array
                     )
-                    run,status = get_run_status(run, client, thread)
-                    
+                    run, status = get_run_status(run, client, thread)
+
                     resp = get_assistant_message(client, thread_id)
                     message = bhashini_output(resp, lang=lang)
                     print('ok7')
-                    
+
                     history = {
-                        "thread_id": thread.id,
+                        "thread_id": thread_id,
                         "run_id": run.id,
                         "status": status,
                     }
-                    history = json.dumps(history)
-                    set_redis(chat_id, history)
+                    set_redis(chat_id, json.dumps(history))
                     return message, history
                 else:
                     return "Complaint not found", history
+                
     return assistant_message, history
 
 def bhashini_text_chat(chat_id, text, lang): #lang
