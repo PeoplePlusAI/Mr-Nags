589826fceb860710c21d25208d87c1ed4112138c
LUV SINGH <luv13singh@gmail.com>
2024-02-14 16:37:45 +0530

    fixed config.yaml

--- utils/bhashini.py
+++ utils/bhashini.py
@@ -3,7 +3,7 @@ import json
 import yaml
 
 def load_config():
-    with open('utils/CONFIG.YAML', 'r') as f:
+    with open('CONFIG.YAML', 'r') as f:
         config = yaml.safe_load(f)
     return config
 
